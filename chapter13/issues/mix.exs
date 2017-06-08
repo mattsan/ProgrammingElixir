e557cb3eb268480862b177b7bb337a46b8e53a91
Eiji MATSUMOTO <e.mattsan@gmail.com>
2017-06-08 21:39:52 +0900

    generate docs

--- chapter13/issues/mix.exs
+++ chapter13/issues/mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.Mixfile do
      escript: escript_config(),
      version: "0.1.0",
      elixir: "~> 1.4",
+     name: "Issues",
+     source_url: "https://github.com/mattsan/ProgrammingElixir",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -31,7 +33,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       httpoison: "~> 0.8",
-      poison: "~> 1.5"
+      poison: "~> 1.5",
+      ex_doc: "~> 0.11",
+      earmark: ">= 0.0.0"
     ]
   end
 
