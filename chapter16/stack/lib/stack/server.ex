7abf22d7f18a7b5116c9cf567e0a1532fcf1d32a
Eiji MATSUMOTO <e.mattsan@gmail.com>
2017-07-27 21:39:28 +0900

    add interface to stack

--- chapter16/stack/lib/stack/server.ex
+++ chapter16/stack/lib/stack/server.ex
@@ -1,6 +1,18 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(init \\ []) do
+    GenServer.start_link(__MODULE__, init, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(value) do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
   def handle_call(:pop, _from, [value|values]) do
     {:reply, value, values}
   end
