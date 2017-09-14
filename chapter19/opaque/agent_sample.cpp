9f29d2151e9a68f40bc7a6458381308ae2959e86
Eiji MATSUMOTO <e.mattsan@gmail.com>
2017-09-15 06:18:26 +0900

    add comment

--- chapter19/opaque/agent_sample.cpp
+++ chapter19/opaque/agent_sample.cpp
@@ -8,18 +8,25 @@ __END__
 #include "agent.h"
 
 int main(int, char* []) {
+    // {:ok, agent} = Agent.start(fn -> 0 end)
     agent_t* agent = agent_start([](){ return 0; });
 
+    // IO.puts Agent.get(agent, &(&1))
     std::cout << agent_get(agent, [](int value) { return value; }) << std::endl;
 
+    // Agent.update(agent, &(&1 + 1))
     agent_update(agent, [](int value) { return value + 1; });
 
+    // Agent.update(agent, &(&1 + 1))
     agent_update(agent, [](int value) { return value + 1; });
 
+    // IO.puts Agent.get(agent, &(&1))
     std::cout << agent_get(agent, [](int value) { return value; }) << std::endl;
 
+    // Agent.update(agent, &(&1 + 99))
     agent_update(agent, [](int value) { return value + 99; });
 
+    // IO.puts Agent.get(agent, &(&1))
     std::cout << agent_get(agent, [](int value) { return value; }) << std::endl;
 
     agent_end(agent);
